17c80290f029891d68c8926384d20b1062b4086e
tuan2090 <tuan72972@gmail.com>
2026-04-02 12:13:37 +0700

    Fix src/libtape.c code

--- src/libtape.c
+++ src/libtape.c
@@ -13,7 +13,7 @@ void init_tape(Tape *tape, size_t size) {
     fprintf(stderr, "ERROR: Couldn't allocate memory for tape\n");
     exit(EXIT_FAILURE);
   }
- 
+
   // Set all tape_data to 0
   memset(tape->tape_data, 0, tape->tape_length);
 
@@ -36,7 +36,7 @@ void free_tape(Tape *tape) {
 
 void move_right(Tape *tape) {
   // Check if tape head is move past tape length
-  if (tape->head >= tape->tape_length-1) {
+  if (tape->head >= tape->tape_length - 1) {
     // Display error messenger
     fprintf(stderr, "ERROR: Head attempted to move past tape length\n");
 
@@ -49,13 +49,14 @@ void move_right(Tape *tape) {
   // If debug mode enable, run it
   if (tape->is_debug_mode_enable) {
     tape->run_step++;
-    printf("Step %zu: Move right (head position: %zu)\n", tape->run_step, tape->head);
+    printf("Step %zu: Move right (head position: %zu)\n", tape->run_step,
+           tape->head);
   }
 }
 
 void move_left(Tape *tape) {
   // Check if tape head is move below index 0
-  if (tape->head == 0) {
+  if (tape->head <= 0) {
     // Display error messenger
     fprintf(stderr, "ERROR: Head attempted to move below index 0\n");
 
@@ -68,46 +69,69 @@ void move_left(Tape *tape) {
   // If debug mode enable, run it
   if (tape->is_debug_mode_enable) {
     tape->run_step++;
-    printf("Step %zu: Move left (head position: %zu)\n", tape->run_step, tape->head);
+    printf("Step %zu: Move left (head position: %zu)\n", tape->run_step,
+           tape->head);
   }
 }
 
 void increment_head_value(Tape *tape) {
+  // Check if tape head is move below index 0
+  if (tape->head >= tape->tape_length - 1) {
+    // Display error messenger
+    fprintf(stderr, "ERROR: Head attempted to move past tape length\n");
+
+    // Exit and send exit failure signal
+    exit(EXIT_FAILURE);
+  }
+
   tape->tape_data[tape->head]++;
 
   // If debug mode enable, run it
   if (tape->is_debug_mode_enable) {
     tape->run_step++;
-    printf("Step %zu: Increment head value (head position: %zu)\n", tape->run_step, tape->head);
+    printf("Step %zu: Increment head value (head position: %zu)\n",
+           tape->run_step, tape->head);
   }
 }
 
 void decrement_head_value(Tape *tape) {
+  // Check if tape head is move below index 0
+  if (tape->head <= 0) {
+    // Display error messenger
+    fprintf(stderr, "ERROR: Head attempted to move below index 0\n");
+
+    // Exit and send exit failure signal
+    exit(EXIT_FAILURE);
+  }
+
   tape->tape_data[tape->head]--;
 
   // If debug mode enable, run it
   if (tape->is_debug_mode_enable) {
     tape->run_step++;
-    printf("Step %zu: Decrement head value (head position: %zu)\n", tape->run_step, tape->head);
+    printf("Step %zu: Decrement head value (head position: %zu)\n",
+           tape->run_step, tape->head);
   }
 }
 
-void move_head(Tape *tape, size_t pos) {
+void head_jump(Tape *tape, size_t pos) {
   // Check if tape head is move below index 0 or past tape length
-  if (tape->head >= tape->tape_length-1 || tape->head == 0) {
+  if (pos >= tape->tape_length - 1 || pos == 0) {
     // Display error messenger
-    fprintf(stderr, "ERROR: Head attempted to move below index 0 or move past tape length\n");
+    fprintf(stderr, "ERROR: Head attempted to move below index 0 or move past "
+                    "tape length\n");
 
     // Exit and send exit failure signal
     exit(EXIT_FAILURE);
   }
 
   tape->head = pos;
-  
+
   // If debug mode enable, run it
   if (tape->is_debug_mode_enable) {
     tape->run_step++;
-    printf("Step %zu: Jump to position %zu (head position: %zu)\n", tape->run_step, pos, tape->head);
+    printf("Step %zu: Jump to position %zu (head position: %zu)\n",
+           tape->run_step, pos, tape->head);
   }
 }
 
@@ -115,7 +139,8 @@ void write_at_head(Tape *tape, unsigned char value) {
   // If debug mode enable, run it
   if (tape->is_debug_mode_enable) {
     tape->run_step++;
-    printf("Step %zu: Write '%c' value at head (head position: %zu)\n", tape->run_step, value, tape->head);
+    printf("Step %zu: Write '%c' value at head (head position: %zu)\n",
+           tape->run_step, value, tape->head);
   }
 
   tape->tape_data[tape->head] = value;
@@ -125,7 +150,8 @@ unsigned char read_at_head(Tape *tape) {
   // If debug mode enable, run it
   if (tape->is_debug_mode_enable) {
     tape->run_step++;
-    printf("Step %zu: Read %zu(%c) value (head position: %zu)\n", tape->run_step, tape->head, tape->tape_data[tape->head], tape->head);
+    printf("Step %zu: Read %zu(%c) value (head position: %zu)\n",
+           tape->run_step, tape->head, tape->tape_data[tape->head], tape->head);
   }
 
   return tape->tape_data[tape->head];
@@ -145,7 +171,8 @@ unsigned char read_tape(Tape *tape, size_t pos) {
   // If debug mode enable, run it
   if (tape->is_debug_mode_enable) {
     tape->run_step++;
-    printf("Step %zu: Read %zu(%c) value (head position: %zu)\n", tape->run_step, pos, tape->tape_data[pos], tape->head);
+    printf("Step %zu: Read %zu(%c) value (head position: %zu)\n",
+           tape->run_step, pos, tape->tape_data[pos], tape->head);
   }
 
   return tape->tape_data[pos];
@@ -158,7 +185,8 @@ void show_tape_table(const Tape *tape) {
 
   for (size_t i = 0; i < tape->tape_length; i++) {
     // Display tape index, tape data (hexadecimal, decimal)
-    printf("%10zu | 0x%02x | %5i | ", i, tape->tape_data[i], tape->tape_data[i]);
+    printf("%10zu | 0x%02x | %5i | ", i, tape->tape_data[i],
+           tape->tape_data[i]);
 
     // Check tape head position
     if (i == tape->head) {
@@ -180,4 +208,3 @@ void show_tape_table(const Tape *tape) {
     printf("\n");
   }
 }
-
